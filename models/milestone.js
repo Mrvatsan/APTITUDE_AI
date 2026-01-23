ab124284b3b81b2afede7c40166beb387d3b1013
Mr.Vatsan <mrvatsan11@gmail.com>
2026-01-23 23:04:58 +0530

    feat(models): define Milestone schema for goal tracking

---
+++ models/milestone.js
@@ -0,0 +1,30 @@
+/**
+ * Milestone Data Model
+ * 
+ * Defines the schema for educational milestones, which serve 
+ * as high-level progress markers for the aptitude platform.
+ * 
+ * @author Aptitude AI Team
+ * @version 1.0.0
+ */
+
+const { DataTypes } = require('sequelize');
+const sequelize = require('./index');
+
+const Milestone = sequelize.define('Milestone', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    }
+}, {
+    timestamps: true,
+    tableName: 'milestones'
+});
+
+module.exports = Milestone;
